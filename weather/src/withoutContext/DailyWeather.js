8f6f84400249b7881e6efd9e59b53b5c3df48b54
michealshanahan <m_shanahan@hotmail.com>
2020-02-19 19:30:58 -0700

    added several API types

--- weather/src/withoutContext/DailyWeather.js
+++ weather/src/withoutContext/DailyWeather.js
@@ -13,10 +13,35 @@ class DailyWeather extends Component {
 
         let { id, weather_state_name, weather_state_abbr, wind_direction_compass, applicable_date, min_temp, max_temp, the_temp, wind_speed, wind_direction, air_pressure, humidity, visibility, predictability } = this.props.daysWeather
 
+        let weatherToday = ()=> {
+            let date = applicable_date.slice(8)
+            let today = new Date().getDate()
+            if(date == today){
+                return(
+                    <div key  = {this.props.id}>
+                        Today
+                    </div>
+                )
+            }else if (date == (today + 1)){
+                return(
+                    <div key = {this.props.id}>
+                        Tomorrow
+                    </div>
+                )
+            }else {
+                return(
+                    <div key = {this.props.id}>
+                        {applicable_date}
+                    </div>
 
-        console.log(this.props)
+                )
+            }
+        }
+
         return(
-            <div>{applicable_date}</div>
+            <div> {weatherToday()}
+            
+            </div>
         )
     }
 }
